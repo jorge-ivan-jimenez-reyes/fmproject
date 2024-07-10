458b93ac9807ee21ec8d344f10755d90c22b1d65
jorge-ivan-jimenez-reyes <0251139@up.edu.mx>
2024-07-10 11:33:35 -0600

    Inicial commit

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Sidebar from './components/Sidebar';
+import './index.css';
+
+const App = () => {
+  return (
+    <Router>
+      <div className="flex-container">
+        <Sidebar />
+        <div className="main-content">
+          <Routes>
+            <Route path="/" element={<div>Dashboard</div>} />
+            <Route path="/user" element={<div>User</div>} />
+            <Route path="/messages" element={<div>Messages</div>} />
+            <Route path="/analytics" element={<div>Analytics</div>} />
+            <Route path="/file-manager" element={<div>File Manager</div>} />
+            <Route path="/cart" element={<div>Cart</div>} />
+            <Route path="/saved" element={<div>Saved</div>} />
+            <Route path="/setting" element={<div>Setting</div>} />
+          </Routes>
+        </div>
+      </div>
+    </Router>
+  );
+};
+
+export default App;
