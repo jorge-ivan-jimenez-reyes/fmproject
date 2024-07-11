90dc06371174d78ff90e0ca94bdc7494b3f998cb
jorge-ivan-jimenez-reyes <0251139@up.edu.mx>
2024-07-10 19:03:38 -0600

    added Usuario page

---
+++ src/pages/Usuario.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Usuario = ({ username }) => {
+  return (
+    <div>
+      <h1>Usuario: {username}</h1>
+      {/* Aquí puedes agregar más contenido relacionado con el usuario */}
+    </div>
+  );
+};
+
+export default Usuario;
